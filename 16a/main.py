d058c12d68e8c58472693e1f1d5adf1d26b8c351
Ethan Swan <eswan18@rocketmail.com>
2021-12-16 01:13:18 -0600

    Get parsing for bit-based operators.

--- 16a/main.py
+++ 16a/main.py
@@ -5,6 +5,7 @@ from parse import shift, parse
 
 #with open('test_input_literal.txt', 'rt') as f:
 with open('test_input_op_2sub.txt', 'rt') as f:
+#with open('test_input.txt', 'rt') as f:
     content = hex2bits(f.read().strip())
     buffer = deque(content)
 print(''.join(list(buffer)))
--- 16a/parse.py
+++ 16a/parse.py
@@ -5,10 +5,12 @@ from bits import bits2int
 
 @dataclass
 class Literal:
+    version: int
     value: int
 
 @dataclass
 class Operator:
+    version: int
     subpackets: list[Literal]
 
 def shift(dq: deque, n: int = 1) -> list:
@@ -24,48 +26,49 @@ def parse_header(header: list[str]) -> tuple[int, int]:
     type_id = bits2int(header[3:])
     return version, type_id
 
-def parse_packet(type_id: int, buffer: deque):
+def parse_literal(version: int, buffer: deque, subpacket: bool) -> Literal:
+    number_bits = []
+    consumed = 6
+    while True:
+        continuation, *number = shift(buffer, 5)
+        number_bits.extend(number)
+        consumed += 5
+        if continuation == '0':
+            break
+    number = int(''.join(number_bits), base=2)
+    result = Literal(version, number)
+    print(f'Parsed {result=}')
+    return result
+
+def parse_packet(version: int, type_id: int, buffer: deque, subpacket: bool = False):
     # Literal
     if type_id == 4:
-        number_bits = []
-        consumed = 6
-        while True:
-            continuation, *number = shift(buffer, 5)
-            number_bits.extend(number)
-            consumed += 5
-            if continuation == '0':
-                overstep = consumed % 4
-                if overstep:
-                    n_ignored = 4 - overstep
-                    shift(buffer, n_ignored)
-                break
-        number = int(''.join(number_bits), base=2)
-        result = Literal(number)
-        print(f'Parsed {result=}')
-        return result
+        lit = parse_literal(version, buffer, subpacket=subpacket)
+        return lit
     # Operator 
     else:
         length_type_id, *_ = shift(buffer, 1)
         if length_type_id == '0':
+            print('Parsing op with bits')
             bit_length = int(''.join(shift(buffer, 15)), base=2)
             new_bits = shift(buffer, bit_length)
             new_buffer = deque(new_bits)
-            subpackets = parse(new_buffer)
-            return Operator(subpackets=subpackets)
+            subpax = []
+            while '1' in new_buffer:
+                subpax.append(parse(new_buffer, subpacket=True))
+            return Operator(version, subpax)
             print(f'Parsed {result=}')
         elif length_type_id == '1':
+            print('Parsing op with subpax')
             n_subpackets = int(''.join(shift(buffer, 11)), base=2)
             return {'S': n_subpackets}
         else:
             raise ValueError
 
-def parse(buffer: deque) -> list['Packet']:
-    pax = []
-    while len(buffer) > 0:
-        print(f'Parsing {"".join(buffer)}')
-        header = shift(buffer, 6)
-        version, type_id = parse_header(header)
-        print(f'{(version, type_id)=}')
-        pax.append(parse_packet(type_id, buffer))
-    assert len(buffer) == 0
-    return nodes
+def parse(buffer: deque, subpacket: bool = False) -> list['Packet']:
+    print(f'Parsing {"".join(buffer)}')
+    header = shift(buffer, 6)
+    version, type_id = parse_header(header)
+    print(f'{(version, type_id)=}')
+    packet = parse_packet(version, type_id, buffer, subpacket)
+    return packet
