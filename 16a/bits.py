0a165fd05a06c3a069737a08b46b1c5ea7bf3e3c
Ethan Swan <eswan18@rocketmail.com>
2021-12-16 00:11:12 -0600

    Fix bug in hex2bits.

--- 16a/bits.py
+++ 16a/bits.py
@@ -1,5 +1,10 @@
 def hex2bits(code: str) -> str:
-    return format(int(code, base=16), 'b')
+    s = format(int(code, base=16), '0b')
+    over_4 = len(s) % 4
+    if over_4:
+        fill = 4 - over_4
+        s = '0' * fill + s
+    return s
 
 def bits2int(code: str) -> int:
     return int(code, base=2)
