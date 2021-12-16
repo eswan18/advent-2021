58bd35b1b11ed0440d2359a2609844dad176c386
Ethan Swan <eswan18@rocketmail.com>
2021-12-16 01:59:15 -0600

    Clean up 16a for 16b.

--- 16a/main.py
+++ 16a/main.py
@@ -7,25 +7,14 @@ with open('input.txt', 'rt') as f:
     content = hex2bits(f.read().strip())
     buffer = deque(content)
 
-results = []
-while '1' in buffer:
-    p = parse(buffer)
-    results.append(p)
-    print(f'found {p=}')
-    # If you consumed a number of bytes that isn't divisible by four, consume a couple more.
-    extra_bits = len(buffer) % 4
-    if extra_bits:
-        shift(buffer, extra_bits)
+result = parse(buffer)
 
-# Assume it's an operator
 def sum_versions(node) -> int:
     if isinstance(node, Literal):
         return node.version
     else:
         return node.version + sum(sum_versions(p) for p in node.subpackets)
 
-r_sum = 0
-for r in results:
-    r_sum += sum_versions(r)
+r_sum = sum_versions(result)
 
 print(r_sum)
