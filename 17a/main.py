067cf1eb33c292b6035af96264323a9376022715
Ethan Swan <eswan18@rocketmail.com>
2021-12-17 22:33:17 -0500

    Get test input working on 17a.

--- 17a/main.py
+++ 17a/main.py
@@ -1,4 +1,5 @@
 from copy import copy
+from itertools import product
 from dataclasses import dataclass
 
 
@@ -40,3 +41,31 @@ class Probe:
             if p.in_target():
                 return True
         return False
+
+    @property
+    def path(self) -> list[tuple[int, int]]:
+        result = []
+        p = copy(self)
+        result.append((p.x, p.y))
+        while p.y > y_min or p.dy > 0:
+            p.step()
+            result.append((p.x, p.y))
+            if p.in_target():
+                break
+        return result
+
+
+dx_range = range(0, x_max)
+# The largest dy to check is an arbitrarily large number I picked.
+dy_range = range(y_min, (10 + y_max + x_min))
+
+all_time_high_y = y_min
+i = 0
+for dx, dy in product(dx_range, dy_range):
+    p = Probe(dx=dx, dy=dy)
+    if p.ever_in_target():
+        high_y = max(y for (x, y) in p.path)
+        print(f'found a high y ({high_y}) at {p}')
+        all_time_high_y = max(high_y, all_time_high_y) 
+
+print(all_time_high_y)
