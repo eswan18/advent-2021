013833bbc1cfa21ed42fc55b2bf5dafd6ad86f2f
Ethan Swan <eswan18@rocketmail.com>
2021-12-19 10:58:55 -0500

    Get rotation working with some confidence.

--- 19a/scanner.py
+++ 19a/scanner.py
@@ -29,18 +29,49 @@ class Scanner:
         '''
         A scanner with the same coords, from the perspective of "up" being N/4 rotations clockwise.
         '''
-        new_beacons = [Coord(-y, x, z) for (x, y, z) in self.beacons]
+        if rotation == 1:
+            new_beacons = [Coord(-y, x, z) for (x, y, z) in self.beacons]
+        elif rotation == 2:
+            new_beacons = [Coord(-x, -y, z) for (x, y, z) in self.beacons]
+        elif rotation == 3:
+            new_beacons = [Coord(y, -x, z) for (x, y, z) in self.beacons]
+        else:
+            raise ValueError
+
         return self.__class__(
             number=self.number,
             beacons=new_beacons,
             location=self.location,
         )
 
+    def facing_back(self) -> 'Scanner':
+        '''
+        A scanner with the same coords, from the perspective of looking backward.
+        '''
+        new_beacons = [Coord(-x, y, -z) for (x, y, z) in self.beacons]
+        return self.__class__(
+            number=self.number,
+            beacons=new_beacons,
+            location=self.location,
+        )
+
+
     def facing_down(self) -> 'Scanner':
         '''
         A scanner with the same coords, from the perspective of "forward" being N/4 rotations down.
         '''
         new_beacons = [Coord(x, z, -y) for (x, y, z) in self.beacons]
+        return self.__class__(
+            number=self.number,
+            beacons=new_beacons,
+            location=self.location,
+        )
+
+    def facing_up(self) -> 'Scanner':
+        '''
+        A scanner with the same coords, from the perspective of "forward" being N/4 rotations up.
+        '''
+        new_beacons = [Coord(x, -z, y) for (x, y, z) in self.beacons]
         return self.__class__(
             number=self.number,
             beacons=new_beacons,
@@ -57,6 +88,29 @@ class Scanner:
             beacons=new_beacons,
             location=self.location,
         )
+
+    def facing_left(self) -> 'Scanner':
+        '''
+        A scanner with the same coords, from the perspective of "forward" being N/4 rotations left.
+        '''
+        new_beacons = [Coord(z, y, -x) for (x, y, z) in self.beacons]
+        return self.__class__(
+            number=self.number,
+            beacons=new_beacons,
+            location=self.location,
+        )
+
+    def all_rotations(self) -> Iterator['Scanner']:
+        facings = (
+            self, self.facing_back(),
+            self.facing_down(), self.facing_up(),
+            self.facing_right(), self.facing_left(),
+        )
+        for f in facings:
+            yield f
+            yield f.rotated(1)
+            yield f.rotated(2)
+            yield f.rotated(3)
 
     def __iter__(self) -> Iterator[Coord]:
         return iter(self.beacons)
