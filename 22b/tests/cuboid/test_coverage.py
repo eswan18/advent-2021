ca121e917caa1a82edb133d888563666bcce092f
Ethan Swan <eswan18@rocketmail.com>
2021-12-24 23:07:46 -0500

    Clean up test of covered volume..

--- 22b/tests/cuboid/test_coverage.py
+++ 22b/tests/cuboid/test_coverage.py
@@ -31,7 +31,6 @@ def test_overlap4():
     b = Cuboid.from_string('x=-20..33,y=-21..23,z=-26..28')
     c = Cuboid.from_string('x=-22..28,y=-29..23,z=-38..16')
     d = Cuboid.from_string('x=-46..7,y=-6..46,z=-50..-1')
-    print(d.uncovered_volume([a, b, c]))
     assert d.uncovered_volume([a, b, c]) == 102852
     assert d.uncovered_volume([c, b, a]) == 102852
 
