30bb0caf62e54b4009a7fbf310c38b935b78a3b9
Ethan Swan <eswan18@rocketmail.com>
2022-01-20 10:04:55 -0600

    Add misdirected approach.

--- 14b/ruby/main.rb
+++ 14b/ruby/main.rb
@@ -3,28 +3,56 @@ FILENAME = "input.txt"
 lines = File.open(FILENAME).readlines.map(&:chomp)
 
 sequence = lines[0].split("")
-rules = Hash[
+Rules = Hash[
   lines[2..].map do |line|
     pair, insertion = line.split(" -> ")
   end
 ]
 
+# A hash of hashes
+Cache = Hash.new{ | hash, key| hash[key] = {} }
+
 class Array
-  def update(rules)
-    new_seq = clone
-    (length-1).times do |i|
-      insertion = rules.fetch(self[i] + self[i+1])
-      new_seq.insert(2*i+1, insertion)
+  def update(times=1)
+    if times == 1
+      new_seq = clone
+      (length-1).times do |i|
+        insertion = Rules.fetch(self[i] + self[i+1])
+        new_seq.insert(2*i+1, insertion)
+      end
+      new_seq
+    elsif times == 10
+      if Cache[10].include?(self)
+        Cache[10][self]
+      else
+        seq = self
+        10.times do
+          seq = seq.update
+        end
+        Cache[10][self] = seq
+      end
+    elsif times == 20
+      if Cache[20].include?(self)
+        Cache[20][self]
+      else
+        result = update(10).update(10)
+        Cache[20][self] = result
+      end
+    else
+      raise RuntimeError "Bad number of times"
     end
-    new_seq
+  end
+  def value_counts
+    counts = Hash.new 0
+    each{|letter| counts[letter] += 1}
+    counts
+  end
+  def max_minus_min
+    counts = value_counts
+    min, max = counts.minmax_by{|k, v| v}
+    max[1] - min[1]
   end
 end
 
-10.times do
-  sequence = sequence.update(rules)
-end
-
-most_common_letter = sequence.max_by{|s| sequence.count(s)}
-least_common_letter = sequence.min_by{|s| sequence.count(s)}
-result = sequence.count(most_common_letter) - sequence.count(least_common_letter)
-p result
+sequence = sequence.update(20)
+p sequence.max_minus_min
